ec7eb1cbee846c24322e04eabec90d8a25b29406
Sagi Merner <smerner@protonmail.com>
2025-06-29 13:38:07 +0300

    Add base react plugins

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />}></Route>
+      <Route path="auth">
+        <Route path="login" element={<Login />}/>  
+        <Route path="register" element={<Register />}/>  
+      </Route>
+
+    </Routes>
+  </BrowserRouter>
+);
