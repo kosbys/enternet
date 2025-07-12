2cd56a636ae669a7c3bf9c928da0c5c7c9bc7b85
kosbys <smerner@protonmail.com>
2025-07-12 22:47:57 +0300

    Add register form but without data

--- client/src/Components/RegisterForm.tsx
+++ client/src/Components/RegisterForm.tsx
@@ -1,1 +1,20 @@
-export default function RegisterForm() {}
+import Joi from "joi";
+import type { RegisterFormData, FormField } from "../types";
+import Form from "./Form";
+
+export default function RegisterForm() {
+  const registerSchema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string(),
+    password: Joi.string(),
+    passwordConfirm: Joi.string(),
+  });
+
+  const formFields: FormField[] = [{}];
+
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
